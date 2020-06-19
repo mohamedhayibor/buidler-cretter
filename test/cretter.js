1bcea6299ca7546bad83759f51c9ece3eb31d0f4
Mohamed Hayibor <mohamedhayibor@gmail.com>
2020-06-19 17:46:33 +0700

    Add Contract deployment params

--- test/cretter.js
+++ test/cretter.js
@@ -26,18 +26,29 @@ describe("Token contract", function() {
     [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
   })
 
+  // Set up tests
   it("Stater must be owner of the contract", async function() {
     const stater = await statement.stater();
     expect(stater).to.equal(await owner.getAddress());
   });
 
+  it("Contract deployment params", async function() {
+    const lastQuestioner = await statement.lastQuestioner();
+    const firstQuestioner = await statement.firstQuestioner();
 
+    expect(lastQuestioner).to.equal(0);
+    expect(firstQuestioner).to.equal(1);
+  });
 
   it("StatementBankBalance must be 0.04 eth at deployment ", async function() {
     const statementBankBalance = await statement.statementBankBalance();
     expect( statementBankBalance.toString() ).to.equal("40000000000000000");
   });
 
-
+  // Interaction tests
+  // 1. addr1 asks question
+  // 2. owner provides answer
+  // 3. addr2 votes down answer
+  // 4. check proper finalization
 
 });
